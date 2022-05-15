55a96ac05a774d897a9d7f861a8930454bf3a2dd
André Marques <82080573+andremfdev@users.noreply.github.com>
2022-05-15 05:34:52 -0300

    Update JS sections active

--- main.js
+++ main.js
@@ -4,6 +4,49 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // 1) verificar se a seção passou da linha
+  // 2) quais dados vou precisar?
+
+  // A) o topo da seção
+  const sectionTop = section.offsetTop
+
+  // B) altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // C) o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // 3) verificar se a base está abaixo da linha alvo
+  // 4) quais dados vou precisar?
+
+  // A) a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // B) o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a [href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnScroll() {
